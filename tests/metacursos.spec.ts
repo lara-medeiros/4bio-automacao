6fd5dae146580ee3961e805548b9be5e45538de7
Lara Medeiros <laramedeiros695@gmail.com>
2025-09-21 16:59:55 -0300

    feat: commit inicial e base do teste de validação das atribuições da meta de curso - processo seletivo 4BIO

---
+++ tests/metacursos.spec.ts
@@ -0,0 +1,30 @@
+const { test, expect } = require('@playwright/test');
+
+test('Adicionar Meta de Curso - Teste de Validação', async ({ request }) => {
+    const baseUrl = 'https://4-bio-processo-seletivo-lara.wiremockapi.cloud';
+
+    const token = 'tokenAuth';
+    const dadosParaEnviar = {
+        Id: -1,
+        DepId: 1,
+        MetaId: 1,
+        AnoVigente: 2024,
+        MetaAnual: true,
+        MetaSemestral: true,
+        DataLimitePreenchimento: '2024-09-30T23:59:59'
+    };
+
+    const response = await request.post(`${baseUrl}/metacursos`, {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        data: dadosParaEnviar
+    });
+
+    const respostaJson = await response.json();
+    const listaDeErros = respostaJson.listaDeErros;
+    const mensagemDeErro = listaDeErros[0].Mensagem;
+
+    expect(mensagemDeErro).toBe('O campo Id requer um valor maior que 0.');
+});
